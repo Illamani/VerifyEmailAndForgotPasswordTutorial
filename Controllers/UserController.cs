d979cd0f702786b51a00efa744526a027372a17a
WB Vera <WBVera@rosariobus.com.ar>
2022-09-12 10:32:26 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using VerifyEmailAndForgotPasswordTutorial.Data;
+using VerifyEmailAndForgotPasswordTutorial.Models;
+
+namespace VerifyEmailAndForgotPasswordTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public UserController(DataContext context)
+        {
+            _dataContext = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegisterRequest request)
+        {
+            if(_dataContext.Users.Any(u=>u.Email == request.Email))
+            {
+                return BadRequest("User Already exist.");
+            }
+            CreatePassWordHash(request.Password,
+                out byte[] passwordHash,
+                out byte[] passwordSalt);
+
+            var user = new User
+            {
+                Email = request.Email,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                VerificationToken = CreateRandomToken()
+            };
+
+            _dataContext.Users.Add(user);
+            await _dataContext.SaveChangesAsync();
+
+            return Ok("User created!");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginRequest request)
+        {
+            var user = await _dataContext.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+            if(user == null)
+            {
+                return BadRequest("User not found");
+            }
+
+            if (!VerifyPassWordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Password is incorrect");
+            }
+
+            if (user.VerifiedAt == null)
+            {
+                return BadRequest("Not Verified");
+            }
+            
+
+            return Ok($"Welcome back, {user.Email}! :)");
+        }
+
+
+        [HttpPost("verify")]
+        public async Task<IActionResult> Verify(string token)
+        {
+            var user = await _dataContext.Users.FirstOrDefaultAsync(u => u.VerificationToken == token);
+            if (user == null)
+            {
+                return BadRequest("Invalid Token");
+            }
+
+            user.VerifiedAt = DateTime.Now;
+            await _dataContext.SaveChangesAsync();
+
+
+            return Ok("User verified!");
+        }
+
+        [HttpPost("forgot-password")]
+        public async Task<IActionResult> ForgotPassword(string email)
+        {
+            var user = await _dataContext.Users.FirstOrDefaultAsync(u => u.Email == email);
+            if (user == null)
+            {
+                return BadRequest("User not found");
+            }
+
+            user.PasswordResetToken = CreateRandomToken();
+            user.ResetTokenExpires = DateTime.Now.AddDays(1);
+            await _dataContext.SaveChangesAsync();
+
+
+            return Ok("You may now reset your password");
+        }
+
+        [HttpPost("reset-password")]
+        public async Task<IActionResult> ResetPassword(ResetPasswordRequest request)
+        {
+            var user = await _dataContext.Users.FirstOrDefaultAsync(u => u.PasswordResetToken == request.Token);
+            if (user == null || user.ResetTokenExpires < DateTime.Now)
+            {
+                return BadRequest("Invalid Token");
+            }
+
+            CreatePassWordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            user.PasswordResetToken = null;
+            user.ResetTokenExpires = null;
+
+            await _dataContext.SaveChangesAsync();
+
+
+            return Ok("Password succesfully reset.");
+        }
+
+
+        private bool VerifyPassWordHash(string password, byte[] passwordHash,byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac
+                    .ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            };
+        }
+
+
+
+        private void CreatePassWordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac
+                    .ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private string CreateRandomToken()
+        {
+            return Convert.ToHexString(RandomNumberGenerator.GetBytes(64));
+        }
+
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VerifyEmailAndForgotPasswordTutorial.Models;
+
+namespace VerifyEmailAndForgotPasswordTutorial.Data
+{
+    public class DataContext : DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options ): base( options )
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder
+                .UseSqlServer("Data Source=desktop-fpdjkd9;Initial Catalog=VerificationEmail;Integrated Security=True");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
